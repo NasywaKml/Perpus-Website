32f270ca2b9f10aa41e58b9706d58c6f12e485d0
DavinoArraya <61465970+DavinoArraya@users.noreply.github.com>
2025-12-03 22:49:38 +0700

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-createApp(App).mount('#app')
+// [CRITICAL] This allows cookies (JSESSIONID) to be sent/received
+axios.defaults.withCredentials = true; 
+// [OPTIONAL] Set base URL globally to avoid typing it everywhere
+axios.defaults.baseURL = "http://localhost:8080"; 
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
